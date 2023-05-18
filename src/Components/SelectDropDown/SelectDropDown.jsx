5f16b5a62155949c18ab8a1988fc7f8063de3925
Dhruv Pasricha <dhruvpasricha0002@gmail.com>
2023-05-18 18:10:51 +0530

    add `Header and Dropdown Component`

---
+++ src/Components/SelectDropDown/SelectDropDown.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import Select from "@mui/material/Select";
+import FormControl from "@mui/material/FormControl";
+
+const SelectDropDown = ({ value, label, setValue, values }) => {
+    return (
+        <FormControl sx={{ width: "150px" }}>
+            <InputLabel
+                sx={{ color: "var(--text-color)", textAlign: "center" }}
+            >
+                {label}
+            </InputLabel>
+            <Select
+                variant="outlined"
+                value={value}
+                label={label}
+                onChange={(event) => {
+                    setValue(event.target.value);
+                }}
+                sx={{
+                    "& .MuiOutlinedInput-notchedOutline": {
+                        border: "none",
+                    },
+                    "& .MuiInputLabel-root": {
+                        display: "none",
+                    },
+                    "& .MuiInputBase-root": {
+                        "&:before": {
+                            borderBottom: "none",
+                        },
+                    },
+                    color: "inherit",
+                }}
+            >
+                {values.map((v) => (
+                    <MenuItem key={v} value={v}>
+                        {v}
+                    </MenuItem>
+                ))}
+            </Select>
+        </FormControl>
+    );
+};
+
+export default SelectDropDown;
--- src/Pages/Room/CodeEditor/CodeEditor.jsx
+++ src/Pages/Room/CodeEditor/CodeEditor.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import AceEditor from "react-ace";
 import "ace-builds/src-noconflict/mode-c_cpp";
 import "ace-builds/src-noconflict/theme-dracula";
@@ -6,35 +6,55 @@ import "ace-builds/src-noconflict/ext-language_tools";
 import "ace-builds/src-noconflict/snippets/c_cpp";
 import Console from "./Console/Console";
 import "./CodeEditor.css";
+import SelectDropDown from "../../../Components/SelectDropDown/SelectDropDown";
+import { fontSizes } from "./Constants";
+
+const Header = ({ fontSize, setFontSize }) => {
+    return (
+        <div className="code-editor-header">
+            <SelectDropDown
+                value={fontSize}
+                setValue={setFontSize}
+                label="Font Size"
+                values={fontSizes}
+            />
+        </div>
+    );
+};
 
 const CodeEditor = ({ handleBodyChange, value }) => {
+    const [fontSize, setFontSize] = useState(16);
+
     return (
         <div className="code-editor">
-            <AceEditor
-                mode="c_cpp"
-                theme="dracula"
-                onLoad={(editor) => {
-                    editor.focus();
-                }}
-                onChange={(newValue) => handleBodyChange(newValue)}
-                value={value}
-                setOptions={{
-                    enableBasicAutocompletion: true,
-                    enableLiveAutocompletion: true,
-                    enableSnippets: true,
-                    showLineNumbers: true,
-                    tabSize: 4,
-                    highlightActiveLine: false,
-                    hScrollBarAlwaysVisible: false,
-                }}
-                fontSize={14}
-                showPrintMargin={false}
-                showGutter={true}
-                highlightActiveLine={false}
-                height="100%"
-                width="100%"
-            />
-            <Console />
+            <Header fontSize={fontSize} setFontSize={setFontSize} />
+            <div style={ {display: "flex", flexDirection: "column", height: "85vh"}}>
+                <AceEditor
+                    mode="c_cpp"
+                    theme="dracula"
+                    onLoad={(editor) => {
+                        editor.focus();
+                    }}
+                    onChange={(newValue) => handleBodyChange(newValue)}
+                    value={value}
+                    setOptions={{
+                        enableBasicAutocompletion: true,
+                        enableLiveAutocompletion: true,
+                        enableSnippets: true,
+                        showLineNumbers: true,
+                        tabSize: 4,
+                        highlightActiveLine: false,
+                        hScrollBarAlwaysVisible: false,
+                    }}
+                    fontSize={fontSize}
+                    showPrintMargin={false}
+                    showGutter={true}
+                    highlightActiveLine={false}
+                    height="100%"
+                    width="100%"
+                />
+                <Console />
+            </div>
         </div>
     );
 };
