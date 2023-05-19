9f33dfac607c2f2b1de0025d38036f34bb55ffef
Dhruv Pasricha <dhruvpasricha0002@gmail.com>
2023-05-19 09:17:36 +0530

    update `User Avatar Hash Values`

--- src/Components/UserAvatar/UserAvatar.jsx
+++ src/Components/UserAvatar/UserAvatar.jsx
@@ -4,11 +4,11 @@ import { Avatar, Tooltip } from "@mui/material";
 const stringToColor = (string) => {
     let hash = 0;
     for (let i = 0; i < string.length; i += 1) {
-        hash = string.charCodeAt(i) + ((hash << 1) - hash);
+        hash = string.charCodeAt(i) + ((hash << 7) - hash);
     }
     let color = "#";
     for (let i = 0; i < 3; i += 1) {
-        const value = (hash >> (i * 3)) & 0xff;
+        const value = (hash >> (i * 8)) & 0xff;
         color += `00${value.toString(16)}`.slice(-2);
     }
     return color;
